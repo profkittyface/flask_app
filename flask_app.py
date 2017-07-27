4606e17acb9819cfcde1b323fd1ade7d2b4939eb
Drew Hunt <ahunt@opaqueresearch.com>
2017-07-27 14:23:31 -0700

    Unsecured update exmaple

--- flask_app.py
+++ flask_app.py
@@ -1,11 +1,17 @@
 #!/usr/bin/env python
 from flask import Flask
+from subprocess import Popen
 
 app = Flask(__name__)
 
 @app.route('/')
 def hello():
     return 'Hello'
+
+@app.route('/update')
+def update_app():
+    p = Popen('git pull'.split(), cwd='/opt/release/flask_app')
+    return 'update initiated'
 
 if __name__ == '__main__':
     app.run()
